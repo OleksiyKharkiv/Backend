27e0abc1c7c29f5032b5e4e8aa1dd7499dd4052c
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2022-10-12 21:45:56 +0200

    12_10_2022

--- src/ArrayApp.java
+++ src/ArrayApp.java
@@ -1,28 +1,34 @@
 public class ArrayApp {
     public static void main(String[] args) {
         int[] arr;
-        arr = new int[10];
-        arr[0] = 11;
-        arr[1] = 12;
-        arr[2] = 13;
-        arr[3] = 14;
-        arr[4] = 51;
-        arr[5] = 32;
-        arr[6] = 16;
-        arr[7] = 17;
-        arr[8] = 18;
-        int searchKey = 51;
-        int i;
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i] == searchKey) {
-                break;
-            }
+        arr = new int[4];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+
+//        int searchKey = 51;
+        int i, j, k;
+        int sumArr = 0;
+        double mean = 0;
+//        for (i = 0; i < arr.length; i++) {
+//            if (arr[i] == searchKey) {
+//                break;
+//            }
+//        }
+//        if (i == arr.length) {
+//            System.out.println("Can't find " + searchKey);
+//        } else {
+//            System.out.println("Found at: " + i);
+//        }
+        for (j = 0; j < arr.length; j++) {
+            sumArr = sumArr + arr[j];
         }
-        if (i == arr.length) {
-            System.out.println("Can't find " + searchKey);
-        } else {
-            System.out.println("Found at: " + i);
-        }
+        System.out.println("Summ= " + sumArr);
+        mean = sumArr/ (arr.length);
+        System.out.println("Mean=" + mean);
+        System.out.println("AL=" + arr.length);
+
 
     }
 }
