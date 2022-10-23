66110e7998cc3232f6873f626fbe85cd6738525c
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2022-10-23 11:46:40 +0200

    BE DZ 20.10.2022

---
+++ src/Alg_Lesson_19_10_2022/Main.java
@@ -0,0 +1,28 @@
+package Alg_Lesson_19_10_2022;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//
+//    public static int SearchDuplicate(int[] arr) {
+//        arr = new int[]{ 5, 2, 7, 7, 5};
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0 ; j < arr.length; j++) {
+//                switch (arr[i]) {
+//                    case i == j:
+//                        break;
+//                    case arr[i]==arr[j]:
+//                        System.out.println("Duplicates is:"+ arr[i] + "index:"+ i + "," +j);
+//                }
+//
+//            }
+//
+//        }
+//
+//
+//        return 0;
+//    }
+//}
