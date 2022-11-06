e88532f1160d56f49249b0b2c658f667f55e79e1
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2022-11-06 17:10:43 +0100

    Motorcycles

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,7 +1,7 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = new int[] {11, 22, 44, 50, 60, 86, 114, 140, 145, 190};
-        int val = 114;
+        int[] arr = new int[] {11, 22, 44, 50, 86, 114, 140, 145, 190};
+        int val = 11;
         int i= arr.length/2;
         int res;
         while (arr[i]!=val) {
@@ -11,7 +11,6 @@ public class BinarySearch {
             if (arr[i] > val) {
                 i = i/2;
             }
-
         }
         res= i;
         System.out.println("index val="+res);
--- src/DZ_27_10_2022/Motorcycles.java
+++ src/DZ_27_10_2022/Motorcycles.java
@@ -2,7 +2,7 @@ package DZ_27_10_2022;
 
 public class Motorcycles {
     private String name;
-    private int power;
+    int power;
     private String color;
     private int engineVolume;
     private int speed;
@@ -26,4 +26,59 @@ public class Motorcycles {
                 ", speed=" + speed +
                 '}';
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(int engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public static <mot1> void main(String[] args) {
+        String Ural = new String();
+        String green = new String();
+        mot1 Motorcycles = new Motorcycles(Ural, 36, green, 650, 105);
+    }
+
+    public static <mot2>void main(String[] args) {
+        String Jawa = new String();
+        String red = new String();
+        mot2 Motorcycles = new Motorcycles(Jawa, 18, red, 350, 150);
+
+    }
+
+
 }
