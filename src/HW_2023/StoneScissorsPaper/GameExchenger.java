8b5f33ef47ed465ff9f06dc539e41b9c240df464
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-01-12 13:14:28 +0100

    Stone&Etc_1.2

--- src/HW_2023/StoneScissorsPaper/GameExchenger.java
+++ src/HW_2023/StoneScissorsPaper/GameExchenger.java
@@ -1,9 +1,9 @@
-package HW_2023.StoneScissorsPaper;
-
-import java.util.concurrent.Exchanger;
-
-public class GameExchenger {
-    Exchanger<Action> exchanger =new Exchanger<>();
-
-    new Person("Alexey Babaykin",  )
-}
+//package HW_2023.StoneScissorsPaper;
+//
+//import java.util.concurrent.Exchanger;
+//
+//public class GameExchenger {
+//    Exchanger<Action> exchanger =new Exchanger<>();
+//
+//    new Person("Alexey Babaykin",  )
+//}
--- src/HW_2023/StoneScissorsPaper/Person.java
+++ src/HW_2023/StoneScissorsPaper/Person.java
@@ -1,15 +1,54 @@
 package HW_2023.StoneScissorsPaper;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.concurrent.Exchanger;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class Person extends Thread {
-    private String name;
+    private String namenick;
     private Action action;
     private Exchanger<Action> exchanger;
     double count = 0;
 
-    public Person(String name, Action action, double count, Exchanger<Action> exchanger) {
-        this.name = name;
+    public void setNamenick(String namenick) {
+        this.namenick = namenick;
+    }
+
+    public String getNamenick() {
+        return namenick;
+    }
+
+    public void setAction(Action action) {
+        this.action = action;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public void setCount(double count) {
+        this.count = count;
+    }
+
+    public double getCount() {
+        return count;
+    }
+
+    public void setExchanger(Exchanger<Action> exchanger) {
+        this.exchanger = exchanger;
+    }
+
+    public Exchanger<Action> getExchanger() {
+        return exchanger;
+    }
+
+    public Person(String namenick, Action action, double count, Exchanger<Action> exchanger) {
+        this.namenick = namenick;
         this.action = action;
         this.count = count;
         this.exchanger = exchanger;
@@ -21,7 +60,7 @@ public class Person extends Thread {
         return Action.values()[random];
     }
 
-    public double whoWins(Action[] myAction, Action[] otherAction) {
+    public static double whoWins(Action[]myAction, Action[]otherAction) {
         double count = 0;
         myAction = Action.values();
         otherAction = Action.values();
@@ -33,5 +72,20 @@ public class Person extends Thread {
             count = count + 1;
         return count;
     }
+    public void run() {
+        Action reply;
+        for (int i = 0; i < ; i++) {
+            
+        }{
+            try {
+                reply = exchanger.exchange(action);
+                whoWins(action, reply);
+                sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 }
